cf15ea062917171d8659ebfd464078b33273f673
Ytalodias <159350706+Ytalodias@users.noreply.github.com>
2025-06-03 19:19:31 -0300

    atividade 4

--- exercicio.js
+++ exercicio.js
@@ -1,65 +1,23 @@
-//1
+const http = require('http');
 
-const contarPares = (n) => {
-  for (let i = 0; i <= n; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
-};
+const servidor = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
 
-
-contarPares(10);
-
-//2
-
-const livro = {
-  titulo: "JavaScript Essencial",
-  autor: "Alana Souza",
-  ano: 2024,
-  sinopse: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-  editora: "Mundo Dev"
-};
-
-
-const { titulo, ano } = livro;
-
-console.log(titulo);
-console.log(ano);   
-
-//3
-const frutas = ["maçã", "banana"];
-const novasFrutas = ["laranja", "abacaxi"];
-
-const todasAsFrutas = [...frutas, ...novasFrutas];
-
-console.log(todasAsFrutas); 
-
-//4
-
-const verificarAcesso = (idade, temIngresso) => {
-  if (idade >= 18 && temIngresso) {
-    console.log("Acesso permitido");
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Bem-vindo ao meu servidor!');
+  } else if (req.url === '/contato') {
+    res.statusCode = 200;
+    res.end('Página de Contato');
+  } else if (req.url === '/servicos') {
+    res.statusCode = 200;
+    res.end('Nossos serviços estão em construção.');
   } else {
-    console.log("Acesso negado");
+    res.statusCode = 404;
+    res.end('Página não encontrada.');
   }
-};
+});
 
-
-verificarAcesso(20, true); 
-verificarAcesso(17, true);  
-
-
-//5
-
-const exibirAviso = (estaChovendo, semGuardaChuva) => {
-  if (estaChovendo || semGuardaChuva) {
-    console.log("Leve um guarda-chuva!");
-  } else {
-    console.log("Tudo certo, pode sair tranquilo");
-  }
-};
-
-
-exibirAviso(true, false);   
-exibirAviso(false, false);  
+servidor.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000/');
+});
