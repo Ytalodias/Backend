427025cf6ce1a5099999d9a86b245152dc17aafd
Ytalodias <159350706+Ytalodias@users.noreply.github.com>
2025-06-10 20:40:20 -0300

    Update index.js

--- ApiUser/index.js
+++ ApiUser/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const usersRoutes = require('./users');
 
-app.use(express.json()); // Permite o uso de JSON
-app.use('/users', usersRoutes); // Usa as rotas de usuários
+app.use(express.json()); 
+app.use('/users', usersRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
